f146b04734d5da55b1148f7e0274b50708ae5aef
eye1905 <cfcyoga@gmail.com>
2019-11-17 17:57:55 +0700

    update posisi

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // untuk hystori
 Route::get('hystori', 'Api\HystoriController@index');
 Route::post('hystori/store', 'Api\HystoriController@store');
