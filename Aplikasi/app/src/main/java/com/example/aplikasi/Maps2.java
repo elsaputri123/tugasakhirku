2843da5914a442c870db13c638cd9cd3e49cc7d5
eye1905 <cfcyoga@gmail.com>
2019-11-22 00:55:24 +0700

    udpate lagi

--- Aplikasi/app/src/main/java/com/example/aplikasi/Maps2.java
+++ Aplikasi/app/src/main/java/com/example/aplikasi/Maps2.java
@@ -42,7 +42,7 @@ public class Maps2 extends FragmentActivity implements OnMapReadyCallback {
     Double x_akhir, y_akhir;
     String id_user, id_nota;
     SharedPreferences pref;
-    String hosts = "http://gabsijawatimur.com";
+    String hosts = "http://192.168.43.148";
     Location mlocation;
 
     @Override
